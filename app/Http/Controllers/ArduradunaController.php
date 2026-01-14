6de837db6cfbc0a174faf2beaaece934afd6f13f
Xbilbao <x_bilbaozam@fpzornotzalh.eus>
2026-01-14 16:17:08 +0100

    created Arduraduna model

---
+++ app/Http/Controllers/ArduradunaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Arduraduna;
+use Illuminate\Http\Request;
+
+class ArduradunaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Arduraduna $arduraduna)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Arduraduna $arduraduna)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Arduraduna $arduraduna)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Arduraduna $arduraduna)
+    {
+        //
+    }
+}
